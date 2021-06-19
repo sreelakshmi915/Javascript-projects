b54ff1ea37b7e3998ad9811b34f14aa38d9b5b1b
sreelakshmi915 <sreelakshmimohan915@gmail.com>
2021-06-19 16:08:41 +0530

    adding games

---
+++ oddeven.js
@@ -0,0 +1,73 @@
+"use strict";
+const prompt = require("prompt-sync")({sigint:true});
+
+
+function findValue(X,P)
+{   let Y =com();
+    let odd_even =check_odd();
+    
+    if ((X+Y)%2 ==0){
+        if(P%2 ==0){
+           console.log ("You wins");
+
+        }
+        else if (odd_even%2==0){
+            console.log("computer wins...you loose!");
+        }
+        else{
+            console.log("tie");
+
+        }
+            
+        
+    }
+    
+    else{
+        if(P%2 !=0){
+           console.log ("You wins");
+
+        }
+        else if (odd_even%2!=0){
+            console.log("computer wins...you loose!");
+        }
+        else{
+            console.log("tie");
+        }
+    }
+        
+    
+    function com(){
+    var Y = Math.floor(Math.random()*10)*1;
+   
+    return Y;
+
+         
+    }
+    function check_odd(){
+        var odd_even = Math.floor(Math.random()*2)*1;
+        return odd_even;
+
+    }
+}
+while(true){
+let  u = prompt("Do you want to play the game odd or even y/n?")
+if(u=='y'){
+let X=prompt("enterplayer1 number PLAYER 1(0-10): ");
+let P=prompt("enterplayer1 number ODD(1) OR EVEN(2): ");
+findValue(X,P);
+
+}
+else if(u=='n'){
+    console.log("thanks");
+    break;
+}
+
+else{
+
+    continue;
+}
+}
+
+ 
+
+
---
+++ stonepaperscissor.js
@@ -0,0 +1,57 @@
+
+"use strict";
+const prompt = require("prompt-sync")({sigint:true});
+
+function rock_game(x){
+    var Y = user_update(x);
+    var C= com_output();
+    if ((Y==0 && C==0 )||(Y==1 && C ==1)||(Y==2 && C==2)){
+        console.log("TIE");
+    }
+    else if((Y ==0 && C==2)||(Y==1 && C==0)||(Y==2&&C==1)){
+        console.log("YOU WINS:)");
+    } 
+    else if ((C ==0 && Y==2)||(C==1 && Y==0)||(C==2&&Y==1)){
+        console.log("you lose computer wins");
+    }
+
+    function com_output(){
+         var com_out = Math.floor(Math.random()*2+1)+0;
+         return com_out;
+    }
+    
+    
+    function user_update(x){
+        if(x=="Rock"){
+            return 0;
+        }
+        if(x=="Paper"){
+            return 1;
+        }
+        if(x=="Scissor"){
+            return 2;
+        }
+    } 
+}
+
+
+
+
+while(true){
+ var u = prompt("You want to play Rock paper and scissors say y/n?");
+
+if (u=="y"){
+    var j = prompt("enter Rock or Paper or Scissor");
+
+    rock_game(j);
+
+}
+else if (u=="n"){
+    console.log("thanks for playing");
+    break;
+}
+else{
+    continue;
+}
+
+}
